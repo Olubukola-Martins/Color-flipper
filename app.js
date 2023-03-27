83a33977ba9e4e36a9a7f5fa56bb34030c67f9ff
Olubukola <19bukola97@gmail.com>
2023-03-27 17:24:21 +0100

    JS for color flipper included

---
+++ app.js
@@ -0,0 +1,13 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+function getRandomNumber(){
+   return Math.floor(Math.random() * colors.length);
+};
+// math.floor() rounds up to the nearest integer, while math.random() generates a random number
+// between 0 and 1.
+btn.addEventListener("click", function(){
+   const randomNumber = getRandomNumber();
+   document.body.style.backgroundColor = colors[randomNumber];
+   color.textContent = colors[randomNumber];
+})
