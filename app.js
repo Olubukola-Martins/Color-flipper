1b3fe1401dd0611f626bd4a11cc76f95115835d9
Olubukola <19bukola97@gmail.com>
2023-03-29 16:31:07 +0100

    styles added and css link added to the hex html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "skyblue", "rgba(102,144,113)", "rgba(132,104,113)"];
 const color = document.querySelector(".color");
 const btn = document.getElementById("btn");
@@ -10,6 +11,7 @@ btn.addEventListener("click", function(){
    const randomNumber = getRandomNumber();
    document.body.style.backgroundColor = colors[randomNumber];
    color.textContent = colors[randomNumber];
+
 });
 
 
